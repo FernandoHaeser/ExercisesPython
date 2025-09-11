58916f061fd253780bad2e8812b9d976374cfa44
Fernando A. Haeser <fernandohaeserr@gmail.com>
2025-09-10 22:56:29 -0300

    Tupla Cores

---
+++ EstruturaDeDados/Tupla5Cores.py
@@ -0,0 +1,13 @@
+#Crie uma tupla com 5 cores e mostre todas elas.
+
+cores = ("Azul", "Verde", "Vermelho", "Preto", "Amarelo")
+
+for cor in cores:
+    print(cor)
+
+"""
+O que acontece no for cor in cores:
+O for pega cada elemento da tupla cores.
+A variável cor recebe esse valor um de cada vez.
+Então o print(cor) mostra o conteúdo.
+"""
