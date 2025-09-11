a8627d954cac0d0fb081d2a0b319d46ae1670735
Fernando A. Haeser <fernandohaeserr@gmail.com>
2025-09-10 22:51:43 -0300

    vezes que letra aparece na frase

---
+++ EstruturaDeDados/VezesLetra.py
@@ -0,0 +1,7 @@
+#Faça um programa que conte quantas vezes aparece uma letra na frase.
+
+frase = input("Digite uma frase: ")
+
+vezesLetra = frase.count("a")
+
+print("\nA letra A, aparece", str(vezesLetra), " na frase.")
