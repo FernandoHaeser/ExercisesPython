e59d13977e12e4ce9e1ec69fa64886b7264cf7ef
Fernando A. Haeser <fernandohaeserr@gmail.com>
2025-09-10 22:36:30 -0300

    tabuada com if/else

---
+++ EstruturasDeControle/Tabuada.py
@@ -0,0 +1,33 @@
+number = int(input("Digite o numero que deseja saber a tabuada: "))
+
+i = 1
+
+if i == 1:
+    print("\n\n", number, "x", i, "=", number * i)
+    i += 1
+if i == 2:
+    print("\n", number, "x", i, "=", number * i)
+    i += 1
+if i == 3:
+    print("\n",number, "x", i, "=", number * i)
+    i += 1
+if i == 4:
+    print("\n",number, "x", i, "=", number * i)
+    i += 1
+if i == 5:
+    print("\n",number, "x", i, "=", number * i)
+    i += 1
+if i == 6:
+    print("\n",number, "x", i, "=", number * i)
+    i += 1
+if i == 7:
+    print("\n",number, "x", i, "=", number * i)
+    i += 1
+if i == 8:
+    print("\n",number, "x", i, "=", number * i)
+    i += 1
+if i == 9:
+    print("\n",number, "x", i, "=", number * i)
+    i += 1
+if i == 10:
+    print("\n",number, "x", i, "=", number * i)
