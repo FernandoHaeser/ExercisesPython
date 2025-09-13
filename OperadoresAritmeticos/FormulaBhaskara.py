2dfee6f543f20771b7f8993b9758771d50cde17c
Fernando A. Haeser <fernandohaeserr@gmail.com>
2025-09-13 00:55:05 -0300

    bhaskara

--- OperadoresAritmeticos/FormulaBhaskara.py
+++ OperadoresAritmeticos/FormulaBhaskara.py
@@ -4,5 +4,17 @@ a = float(input('Informe o valor A da equacao: '))
 b = float(input('Informe o valor B da equacao: '))
 c = float(input('Informe o valor C da equacao: '))
 
-delta = math.sqrt((b*b) - 4 * a * c)
-resultPlus = (b * (-1)) 
+delta = (b*b) - 4 * a * c
+
+if (delta < 0):
+    print('\nDelta é negativo, a equação não possui raizes reais.')
+    exit()
+
+deltaR = math.sqrt(delta)
+
+resultPlus = (b * (-1)) + deltaR / (2 * a)
+resultMinus = (b * (-1)) - deltaR / (2 * a)
+
+print(f'\nO valor de delta é: {deltaR:.2f}')
+print(f'O valor de X1 é: {resultPlus:.2f}')
+print(f'O valor de X2 é: {resultMinus:.2f}')
